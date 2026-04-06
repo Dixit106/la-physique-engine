8b89337d5f5017dbeb8c6eacb9687de67af55129
Dixit106 <dixitk2958@gmail.com>
2026-04-06 19:52:16 +0530

    falling bouncing particle code starts gravity-kun

--- src/particle.py
+++ src/particle.py
@@ -6,11 +6,22 @@ class Particle:
         self.position = pygame.math.Vector2(x, y)
         self.velocity = pygame.math.Vector2(0, 0)
         self.acceleration = pygame.math.Vector2(0, 0)
-        self.radius = radius 
+        self.radius = radius
+        self.mass = 1.0 
+
+    def apply_force(self, force):   
+        #F=ma, so a=F/m
+        self.acceleration += force / self.mass   
 
     def update(self):
+        #Gravity(push or pull ? )
+        gravity = pygame.math.Vector2(0, 0.5)
+        self.apply_force(gravity)
+
         #Velocity changes postion (no shit sherlock)
-        self.position += self.velocity 
+        self.position += self.velocity
+        self.velocity += self.acceleration 
+        self.acceleration *= 0 #To reset acceleration for the next frame
 
     def draw(self, screen):
         #Drawing a circle to represent particle
