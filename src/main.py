26fb751d541aa9a6c4617e79523f9ccedaf1bd3a
Dixit106 <dixitk2958@gmail.com>
2026-04-07 16:50:54 +0530

    two balls going crazy 00

--- src/main.py
+++ src/main.py
@@ -1,20 +1,23 @@
 #importing
 import pygame
 from sys import exit
-from particle import Particle 
+from particle import Particle
+from spring import Spring  
 
 pygame.init()
 #display surface
 screen = pygame.display.set_mode((800,400))
 #the title 
-pygame.display.set_caption('Physics Engine')
+pygame.display.set_caption('Jelly Engine')
 #for setting max frame rate
 clock = pygame.time.Clock()
 
 #Test Particle
-p1 = Particle(100, 200, 20)
-#Movement
-p1.velocity.x = 2 
+p1 = Particle(300, 100, 20)
+p2 = Particle(450, 100, 20)
+
+#Linking the spring
+link = Spring(p1, p2, 100, 0.05)
 
 while True:
     #to close window or else it's stuck
@@ -26,11 +29,17 @@ while True:
 
     screen.fill((0, 0, 0))
 
+    #will have to link the spring first so it applies forces
+    link.update()
+
     #to draw all elements
     #update everything
     p1.update(800, 400)
+    p2.update(800, 400)
     #setting max frame rate
+    link.draw(screen)
     p1.draw(screen)
+    p2.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
--- src/spring.py
+++ src/spring.py
@@ -2,25 +2,36 @@ import pygame
 
 class Spring:
     def __init__(self, p1, p2, rest_length, stiffness):
-        def __init__(self, p1, p2, rest_length, stiffness):
-            self.p1 = p1 
-            self.p2 = p2 
-            self.rest_length = rest_length 
-            self.stiffness = stiffness 
+        self.p1 = p1 
+        self.p2 = p2 
+        self.rest_length = rest_length 
+        self.stiffness = stiffness 
 
-        def update(self):
-            #Finding the distance and direction b/t two particles
-            direction = self.p2.position - self.p1.position
-            distance = direction.length()
+    def update(self):
+        #Finding the distance and direction b/t two particles
+        direction = self.p2.position - self.p1.position
+        distance = direction.length()
 
-            if distance == 0:
-                return #This will prevent a math error
+        if distance == 0:
+            return #This will prevent a math error
 
-            #How much will it stretch or squish
-            displacement = distance - self.rest_length 
+        #How much will it stretch or squish
+        displacement = distance - self.rest_length 
 
-            #Hooke's Law F= stiffness * displacement so:-
-            force_magnitude = self.stiffness * displacement 
+        #Hooke's Law F= stiffness * displacement so:-
+        force_magnitude = self.stiffness * displacement 
 
-            #To turn direction into vector of length 1
-            direction = direction.normalize()    
+        #To turn direction into vector of length 1
+        direction = direction.normalize()
+
+        #actual force vector
+        force = direction * force_magnitude 
+
+        #newtons 3rd law
+        self.p1.apply_force(force)
+        self.p2.apply_force(-force)
+
+    def draw(self, screen):
+        #white line to represent the spring
+        pygame.draw.line(screen, (255, 255, 255), self.p1.position, self.p2.position, 2)
+
